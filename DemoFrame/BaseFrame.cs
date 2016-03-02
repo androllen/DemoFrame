b8e1a8b3f4dbfa1eeef10580c9e93380493520ba
androllen <androllen@hotmail.com>
2016-03-02 18:51:09 +0800

    fix issue with basepage

---
+++ DemoFrame/BaseFrame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace DemoFrame
+{
+    public abstract class BaseFrame : INotifyFrameChanged
+    {
+        public event EventHandler<BackRequestedEventArgs> BackKeyPressing;
+        public event EventHandler NotifyLeftFramePage;
+        public event EventHandler NotifyRightFramePage;
+
+
+    }
+}
---
+++ DemoFrame/INotifyFrameChanged.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace DemoFrame
+{
+    interface INotifyFrameChanged
+    {
+        /// <summary>
+        ///  当注册页面启动的时候发起
+        /// </summary>
+        event EventHandler NotifyLeftFramePage;
+        /// <summary>
+        /// 当注册页面结束的时候发起
+        /// </summary>
+        event EventHandler NotifyRightFramePage;
+
+        event EventHandler<BackRequestedEventArgs> BackKeyPressing;
+    }
+}
